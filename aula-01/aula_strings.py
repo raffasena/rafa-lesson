3376a2fef9f1768175e4bbe290bb209ed1416288
leomenezessz <leonardosmenezes.ssz@gmail.com>
2020-12-10 22:29:22 -0300

    Desafio 01

---
+++ aula-01/aula_strings.py
@@ -0,0 +1,28 @@
+# Conversão de Dados String para Inteiro
+
+num = 1
+num2: str = "1"
+
+print(f"# Conversão de Dados String para Inteiro : {num + int(num2)}")
+
+# Conversão de Dados Int para String
+
+print(f"# Conversão de Dados Int para String : {str(num) + num2}")
+
+# Atribuição de Valor
+
+print(num)
+
+num = 10
+
+print(num)
+
+num2 = "100"
+
+print(num2)
+
+
+# Quantidade de caracteres de uma string
+
+print(f"Length do num2 : {num2.__len__()}")
+
---
+++ aula-01/conditionals_module.py
@@ -0,0 +1,9 @@
+string_01 = "FFF"
+string_02 = "DDv"
+
+if string_01.__len__() == string_02.__len__():
+    print("As string tem a mesma quantidade de caracteres!")
+else:
+    print("As string não tem a mesma quantidade de caracteres!")
+
+# 1. Quero verificar se as strings são iguais ??
